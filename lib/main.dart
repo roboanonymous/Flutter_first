5e9998d7c7e42dc3d833c4aba7a8bf8735932022
roboanonymous <jsamyak591@gmail.com>
2023-04-19 10:10:33 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,40 @@ void main() {
      MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueAccent,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient:  LinearGradient(
-              colors: [
-                Color.fromARGB(255, 52, 117, 230),
-                Color.fromARGB(255, 43, 85, 157),
-                Color.fromARGB(255, 19, 41, 78)
-              ]
-            ),
-          ),
-          child: const Center(
-            child: Text("Hello World"),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
 
 }
 
+
+class GradientContainer extends StatelessWidget
+{
+  @override
+  Widget build(context)
+  {
+    return Container(
+          decoration: const BoxDecoration(
+            gradient:  LinearGradient(
+              colors: [
+                Color.fromARGB(255, 52, 117, 230),
+                Color.fromARGB(255, 43, 85, 157),
+                Color.fromARGB(255, 19, 41, 78)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text("Hello World", 
+            style: TextStyle(
+            color: Colors.white , 
+            fontSize: 30,
+            )
+            ),
+          ),
+        );
+
+  }
+}
